48555bfcad644463ce35aee1deec3901f408d19e
Harsha Vardhan Reddy <mamidi.harsha.reddy+github@gmail.com>
2024-08-25 11:18:40 +0530

    adjusted the base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === "serve" ? "/" : "/resume",
+  base: command === "serve" ? "/" : "/portfolio",
 }));
